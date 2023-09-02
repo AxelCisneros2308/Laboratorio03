4ab489a7ac15160ef31af9045e0f8bf8d06b4704
AxelCisneros2308 <142689553+AxelCisneros2308@users.noreply.github.com>
2023-09-02 13:42:22 -0500

    Add files via upload

---
+++ src/laboratorio03/Laboratorio03.java
@@ -0,0 +1,16 @@
+package laboratorio03;
+
+public class Laboratorio03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        FrameListaPersona pantalla = 
+                new FrameListaPersona();
+        pantalla.setLocationRelativeTo(null);
+        pantalla.setVisible(true);
+    }
+    
+}
